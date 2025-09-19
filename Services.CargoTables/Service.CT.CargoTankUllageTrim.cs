83e251dd5b04d80629cf5249f7ad15e07ae72fb9
Даниил Поляков <danil.poliakov41@gmail.com>
2025-09-19 15:32:09 +0300

    Перенос интерфейса таблицы из отдельного проекта в проект моделей таблиц

--- Services.CargoTables/Service.CT.CargoTankUllageTrim.cs
+++ Services.CargoTables/Service.CT.CargoTankUllageTrim.cs
@@ -2,5 +2,21 @@
 
 public class Service_CT_CargoTankUllageTrim
 {
-    
+    // static public void TempSaveAll()
+    // {
+    //     string[] input = { "cot1P.txt", "cot1S.txt", "cot2P.txt", "cot2S.txt", "cot3P.txt", "cot3S.txt", "cot4P.txt",
+    //         "cot4S.txt","cot5P.txt","cot5S.txt","cot6P.txt","cot6S.txt", "SlopP.txt","SlopS.txt" };
+    //     string[] output = { "UllageTrimTableCOT1P.bin", "UllageTrimTableCOT1S.bin", "UllageTrimTableCOT2P.bin", "UllageTrimTableCOT2S.bin",
+    //         "UllageTrimTableCOT3P.bin","UllageTrimTableCOT3S.bin","UllageTrimTableCOT4P.bin","UllageTrimTableCOT4S.bin","UllageTrimTableCOT5P.bin",
+    //         "UllageTrimTableCOT5S.bin","UllageTrimTableCOT6P.bin","UllageTrimTableCOT6S.bin","UllageTrimTableSlopP.bin","UllageTrimTableSlopS.bin"};
+    //
+    //     for (int i = 0; i < input.Length; ++i)
+    //     {
+    //         Table_CargoTankUllageTrim.Save_to_file(input[i], output[i]);
+    //         List<Value_Table_CargoTankUllageTrim> cot1 = Table_CargoTankUllageTrim.Read_from_file(output[i]);
+    //
+    //
+    //     }
+    //
+    // }
 }
