3a0c9cedc132f205f8bee5c6bf87e5b29856df39
Даниил Поляков <danil.poliakov41@gmail.com>
2025-09-20 15:29:09 +0300

    Добавлена модель состояния судна

--- Models.LoadingCondition/LoadingConditionTable.cs
+++ Models.LoadingCondition/LoadingConditionTable.cs
@@ -2,6 +2,6 @@
 
 public class LoadingConditionTable
 {
-    List<ILoadingConditionItem> Table { get; set; }
+    public List<ILoadingConditionItem> Table { get; set; }
     public void Add(ILoadingConditionItem item) => Table.Add(item);
 }
---
+++ Models.ShipCondition/ShipCondition.cs
@@ -0,0 +1,31 @@
+﻿namespace PetCargoProgram.Models.ShipCondition;
+
+public class ShipCondition
+{
+    public double LightWeight {get; set;}
+    public double DeadWeight{get; set;}
+    public double Displacement{get; set;}
+    public double DeadWeightRegistred{get; set;}
+
+    public double CargoOnBoard{get; set;}
+    public double BallasrOnBoard{get; set;}
+    public double FuelOilOnBoard{get; set;}
+    public double DieselOnBoard{get; set;}
+    public double FreshWaterOnBoard{get; set;}
+    public double LubeOilOnBoard{get; set;}
+
+    public double DraftAft{get; set;}
+    public double DraftAftPependicular{get; set;}
+    public double DraftFwd{get; set;}
+    public double DraftFwdPependicular{get; set;}
+    public double DraftMean{get; set;}
+
+    public double TCG{get; set;}
+    public double LCG{get; set;}
+    public double VCG{get; set;}
+    public double Gm {get; set;}
+    public double Gom {get; set;}
+
+    public double ShearingForce{get; set;}
+    public double BendingMoment{get; set;}
+}
