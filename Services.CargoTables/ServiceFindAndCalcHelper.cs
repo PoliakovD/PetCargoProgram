7aa6d50f80ab6bf32e8be0668a545c3aac79ffa8
Даниил Поляков <danil.poliakov41@gmail.com>
2025-09-21 08:35:37 +0300

    Добавлен статический класс Find andCalcHelper, в который вынесены все необходимые методы для работы с интерполяцией и экстраполяцией

---
+++ Services.CargoTables/ServiceFindAndCalcHelper.cs
@@ -0,0 +1,22 @@
+﻿namespace PetCargoProgram.Services.CargoTables;
+
+public static class ServiceFindAndCalcHelper
+{
+    public static double GetInterpolatedValue(
+        double calcValue1, double calcValue2,
+        double currentValue,
+        double searchValue1, double searchValue2)
+    {
+        double koef = (calcValue1 - currentValue) / (calcValue1 - calcValue2);
+        return koef * (searchValue2 - searchValue1) + searchValue1;
+    }
+    public static double GetInterpolatedValueByKoef(double koef,
+        double searchValue1, double searchValue2)
+        =>koef * (searchValue2 - searchValue1) + searchValue1;
+    public static double GetUpExtrapoladedValueByKoef(double koef,
+        double searchValue1, double searchValue2)
+        =>koef * (searchValue1 - searchValue2) + searchValue1;
+    public static double GetDownExtrapoladedValueByKoef(double koef,
+        double searchValue1, double searchValue2)
+        => searchValue1 - koef * (searchValue1 - searchValue2);
+}
