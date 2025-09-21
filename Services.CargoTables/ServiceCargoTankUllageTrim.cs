0490993bf64012ea0860cc029c4ba725978df5b7
Даниил Поляков <danil.poliakov41@gmail.com>
2025-09-21 03:09:33 +0300

    Добавление в сервис CargoUllageTrim возможности поиска обьема по значениям пустоты и дифферента

--- Services.CargoTables/ServiceCargoTankUllageTrim.cs
+++ Services.CargoTables/ServiceCargoTankUllageTrim.cs
@@ -13,10 +13,11 @@ public class ServiceCargoTankUllageTrim
         Tables= cargoTankUllageTrim.Tables;
     }
 
-    public double GetVolume(string Name, double Ullage, double Trim)
+    public double GetVolumeWithTrim(string Name, double Ullage, double Trim)
     {
-        double result = 0;
-
+        if(Ullage<0) throw new Exception("Пустота не может быть меньше нуля");
+        if (Trim>8) throw new Exception("Trim не может быть больше 8");
+        if(Trim<-3 throw new Exception("Trim не может быть меньше -3");
         // Выбераем таблицу для нужного танка
         var table = Tables.FirstOrDefault(x => x.Name == Name);
 
@@ -25,16 +26,59 @@ public class ServiceCargoTankUllageTrim
             .OrderBy(n => Math.Abs(n.Ullage - Ullage))
             .Take(2);
 
+        var closestFirst = closest.First();
+        var closestLast = closest.Last();
+
         //расчет коэффициента для интерполяции по пустоте
-        var koef = 1.0;
-        var ullageFirst = closest.First().Ullage;
-        var ullageLast = closest.Last().Ullage;
-        if (closest.First().Ullage != Ullage) koef = (ullageLast-ullageFirst)/(Ullage-ullageFirst);
+        var koef = 0.0;
+        var ullageFirst = closestFirst.Ullage;
+        var ullageLast = closestLast.Ullage;
+        if (ullageFirst != Ullage) koef = (ullageLast-ullageFirst)/(Ullage-ullageFirst);
 
         //Расчет объема по дифференту
-//TODO Доделать!!!
+        Dictionary<int, double> trimsVolumes = [];
 
+        trimsVolumes.Add(-1, GetInterpolatedValueByKoef(koef,
+            closestFirst.CargoVolumeTrim_1,
+            closestLast.CargoVolumeTrim_1));
+        trimsVolumes.Add(0, GetInterpolatedValueByKoef(koef,
+            closestFirst.CargoVolumeTrim0,
+            closestLast.CargoVolumeTrim0));
+        trimsVolumes.Add(1, GetInterpolatedValueByKoef(koef,
+            closestFirst.CargoVolumeTrim1,
+            closestLast.CargoVolumeTrim1));
+        trimsVolumes.Add(2, GetInterpolatedValueByKoef(koef,
+            closestFirst.CargoVolumeTrim2,
+            closestLast.CargoVolumeTrim2));
+        trimsVolumes.Add(3, GetInterpolatedValueByKoef(koef,
+            closestFirst.CargoVolumeTrim3,
+            closestLast.CargoVolumeTrim3));
+        trimsVolumes.Add(4, GetInterpolatedValueByKoef(koef,
+            closestFirst.CargoVolumeTrim4,
+            closestLast.CargoVolumeTrim4));
 
-        return result;
+        return GetIterpolatedValueInDictionary(trimsVolumes,
+            Trim);
+    }
+
+    private static double GetInterpolatedValue(
+        double calcValue1, double calcValue2,
+        double currentValue,
+        double SearchValue1, double SearchValue2)
+    {
+        double koef = (calcValue1 - currentValue) / (calcValue2 - calcValue1);
+        return koef * (SearchValue2 - SearchValue1) + SearchValue1;
+    }
+    private static double GetInterpolatedValueByKoef(double koef,
+        double SearchValue1, double SearchValue2)
+    =>koef * (SearchValue2 - SearchValue1) + SearchValue1;
+
+    private static double GetIterpolatedValueInDictionary(Dictionary<int,double> dictionary, double SearchValue)
+    {
+        // находим два ближайших значения
+        var closest = dictionary.OrderBy(x => Math.Abs(x.Key - SearchValue)).Take(2);
+        var koef = (closest.First().Key-SearchValue)/(closest.First().Key - closest.Last().Key);
+        //Интерполируем
+        return GetInterpolatedValueByKoef(koef,closest.First().Value,closest.Last().Value);
     }
 }
