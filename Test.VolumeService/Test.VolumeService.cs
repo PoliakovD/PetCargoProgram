d8d22b596dd48873197187e70435be5799e7b7d4
Даниил Поляков <danil.poliakov41@gmail.com>
2025-09-21 08:36:00 +0300

    Добавлен тест для VolumeTables

---
+++ Test.VolumeService/Test.VolumeService.cs
@@ -0,0 +1,24 @@
+﻿using PetCargoProgram.DataAccess;
+using PetCargoProgram.Models.CargoTables;
+using PetCargoProgram.Services.CargoTables;
+
+AllCargoTables _cargoTables = new AllCargoTables();
+BinaryCTService.Load(ref _cargoTables);
+
+var tableService = new ServiceVolume(_cargoTables.TablesVolume);
+
+string Name = "COT 1P";
+double Volume = 2000.0;
+
+
+var LCG = tableService.GetLCG(Name, Volume);
+var TCG = tableService.GetTCG(Name, Volume);
+var VCG = tableService.GetVCG(Name, Volume);
+var IY = tableService.GetIY(Name, Volume);
+
+Console.WriteLine("LCG: " + LCG);
+Console.WriteLine("TCG: " + TCG);
+Console.WriteLine("VCG: " + VCG);
+Console.WriteLine("IY: " + IY);
+Console.WriteLine("Volume: " + Volume);
+Console.ReadKey();
