59ba37508d83a57bfbdbbe6c5bf95a0a318bb173
Даниил Поляков <danil.poliakov41@gmail.com>
2025-09-21 03:10:05 +0300

    Тестирование сервиса CargoTankUllageTrim

---
+++ Test.CargoTables/TestCargoTankUllageTrim.cs
@@ -0,0 +1,44 @@
+﻿
+using PetCargoProgram.DataAccess;
+using PetCargoProgram.Models.CargoTables;
+using PetCargoProgram.Services.CargoTables;
+
+AllCargoTables _cargoTables = new AllCargoTables();
+BinaryCTService.Load(ref _cargoTables);
+
+
+// test values
+string Name = "COT 1P";
+double trim = 3.2;
+double ullage = 5.5;
+
+var tableService = new ServiceCargoTankUllageTrim(_cargoTables.TablesCargoTankUllage);
+
+double Volume = tableService.GetVolumeWithTrim(Name, ullage, trim);
+Console.WriteLine("Volume: " + Volume+ "// 6334.68 - OK"); // 6334.68 - OK
+
+// test values 2
+Name = "COT 1P";
+trim = 3.0;
+ullage = 5.5;
+
+Volume = tableService.GetVolumeWithTrim(Name, ullage, trim);
+Console.WriteLine("Volume: " + Volume+ "// 6333.5 - OK"); // 6333.5 - OK
+
+// test values 3
+Name = "COT 1P";
+trim = 4.0;
+ullage = 5.5;
+
+Volume = tableService.GetVolumeWithTrim(Name, ullage, trim);
+Console.WriteLine("Volume: " + Volume+ "// 6339.4 - OK"); // 6339.4 - OK
+
+// test values 4
+Name = "COT 1P";
+trim = 0.0;
+ullage = 5.5;
+
+Volume = tableService.GetVolumeWithTrim(Name, ullage, trim);
+Console.WriteLine("Volume: " + Volume+ "");
+
+Console.ReadKey();
