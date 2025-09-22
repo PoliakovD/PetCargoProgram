b8ee5b779a1f429c910518abd2cd6f547e7999d8
Даниил Поляков <danil.poliakov41@gmail.com>
2025-09-22 15:34:39 +0300

    Добавлен метод Clear, исправлен метод ToString

--- Models.Tanks/CargoTank.ToString.cs
+++ Models.Tanks/CargoTank.ToString.cs
@@ -18,16 +18,7 @@ public partial class CargoTank : ILoadingConditionItem, IEquatable<CargoTank>
         {
             var name = propertyInfo.Name;
             var value = propertyInfo.GetValue(this);
-
-            if (propertyInfo.PropertyType.IsGenericType)
-            {
-                var list = (List<string>)value;
-                sb.AppendLine($"\t{name}: [{string.Join(", ", list)}]");
-            }
-            else
-            {
-                sb.AppendLine($"\t{name}: {value}");
-            }
+            sb.AppendLine($"\t{name}: {value}");
         }
         sb.AppendLine("}");
         return sb.ToString();
--- Models.Tanks/CargoTank.cs
+++ Models.Tanks/CargoTank.cs
@@ -27,5 +27,28 @@ public partial class CargoTank : ILoadingConditionItem, IEquatable<CargoTank>
     public double ObservedVolume{ get; set; }
     public double GrossVolume{ get; set; }
 
+    public void Clear()
+    {
+        Name = null;
+        MaxVolume = 0;
+        Level = 0;
+        Ullage = 0;
+        Volume = 0;
+        VolumePercent = 0;
+        Density = 0;
+        Weight = 0;
+        LCG = 0;
+        VCG = 0;
+        VCG = 0;
+        TCG = 0;
+        TempCelsius = 0;
+        TempFaringates = 0;
+        API = 0;
+        VolumeCorrectionFactorBBLS = 0;
+        VolumeCorrectionFactor = 0;
+        ObservedVolume = 0;
+        GrossVolume = 0;
+    }
     // TODO Добавить свойства для грузового танка (вес груза в вакууме, обьем в баррелях и тд)
 }
+
