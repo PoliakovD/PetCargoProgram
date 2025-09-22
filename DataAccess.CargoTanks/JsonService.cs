7a9226a642eaf1a00cd07e88f2a53a2bb73abaef
Даниил Поляков <danil.poliakov41@gmail.com>
2025-09-22 15:37:43 +0300

    Добавление в слой DataAccess доступа к CargoTables

---
+++ DataAccess.CargoTanks/JsonService.cs
@@ -0,0 +1,48 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Encodings.Web;
+using System.Text.Json;
+using PetCargoProgram.Models.Tanks;
+
+
+namespace PetCargoProgram.DataAccess;
+// TODO Проверка загруженных значений грузового танка
+public static class JsonService
+{
+    public static void Save(IEnumerable<CargoTank> cargoTanks, string path = "CargoTanks.json")
+    {
+        try
+        {
+            var options = new JsonSerializerOptions
+            {
+                Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping,
+                WriteIndented = true
+            };
+            var json = JsonSerializer.Serialize(cargoTanks, options);
+            File.WriteAllText(path, json);
+        }
+        catch (Exception e)
+        {
+            throw new SaveToJsonException(path, e);
+        }
+    }
+
+    public static IEnumerable<CargoTank> Load(string path = "CargoTanks.json")
+    {
+        try
+        {
+            var json = File.ReadAllText(path);
+            var teachers =  JsonSerializer.Deserialize<IEnumerable<CargoTank>>(json);
+
+            if (teachers is null) throw new LoadFromJsonException(path);
+
+            return teachers;
+        }
+        catch (Exception e)
+        {
+            throw new LoadFromJsonException(path, e);
+        }
+    }
+}
