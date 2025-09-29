22bcf8da8e7065f7c04a53c0ee4e366206f2dbe7
Даниил Поляков <danil.poliakov41@gmail.com>
2025-09-29 05:25:38 +0300

    View Code Optimizations

---
+++ PetCargoProgram/Components/OutputValue.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace PetCargoProgram.Components;
+
+public partial class OutputValue : UserControl
+{
+    // Title
+    public static readonly DependencyProperty TitleProperty =
+        DependencyProperty.Register(nameof(Title), typeof(string), typeof(OutputValue));
+    public string Title
+    {
+        get => (string)GetValue(TitleProperty);
+        set => SetValue(TitleProperty, value);
+    }
+
+    // Value
+    public static readonly DependencyProperty ValueProperty =
+        DependencyProperty.Register(nameof(Value), typeof(string), typeof(OutputValue));
+    public string Value
+    {
+        get => (string)GetValue(ValueProperty);
+        set => SetValue(ValueProperty, value);
+    }
+
+
+
+
+    public OutputValue()
+    {
+        InitializeComponent();
+    }
+}
+
